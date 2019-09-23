c0d240d75b8946117634ceebf33b512d8f451c00
solophone <solophone@aliyun.com>
2019-09-23 22:18:11 +0800

    change HashRouter to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import { GlobalStyle } from './style'
 import { IconStyle } from './assets/iconfont/iconfont'
 import { renderRoutes } from 'react-router-config' //renderRoutes读取路由配置转化为Route标签
 import routes from './routers'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <GlobalStyle></GlobalStyle>
       <IconStyle></IconStyle>
       {renderRoutes(routes)}
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
